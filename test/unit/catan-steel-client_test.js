fdfac097e9d4e3461cf1b86c9acfb471e60d2d75
Javier Mendiara Cañardo <javier@tid.es>
2013-10-15 10:52:34 +0200

    IMPLEMENT protocol test stub

--- test/unit/catan-steel-client_test.js
+++ test/unit/catan-steel-client_test.js
@@ -35,9 +35,28 @@ describe('Catan Steel Client', function() {
         expect(client.config).to.be.deep.equal(catanClient._defaultConfig);
       });
     });
+
+    describe('Catan Server Backend', function() {
+      it('should be able to create a server backend implementation for a client');
+
+      describe('Protocol configuration', function() {
+        it('should ear "¡Hola Don Pepito!"');
+        it('should say "¡Hola Don Jose!"');
+        it('should ear "¿Paso usted por mi casa?"');
+        it('should go to don Jose House');
+        it('should say "Por su casa yo pase"');
+        it('should ear "¿Vio usted a mi abuela?"');
+        it('should go to Abuela House');
+        it('should say "A su abuela yo la vi"');
+        it('should ear "¡Adios Don Pepito!"');
+        it('should say "¡Adios Don Jose!"');
+
+      });
+    });
   });
 
   describe('Chopping', function() {
+
     it('should be able to chop steel');
 
     it('should inform when a chop has happened');
