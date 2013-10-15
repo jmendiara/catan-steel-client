9a9978b3ead6a0425c0d3109b9858bf788709f9b
Javier Mendiara Cañardo <javier@tid.es>
2013-10-15 09:31:16 +0200

    IMPLEMENT Catan functional test stub

--- test/unit/catan-steel-client_test.js
+++ test/unit/catan-steel-client_test.js
@@ -1,10 +1,20 @@
 'use strict';
 
-var tdigitalNodeBoilerplate = require('../../');
+describe('Catan Steel Client', function() {
 
-describe('Proof of concept', function() {
-  it('should be awesome', function() {
-    expect(tdigitalNodeBoilerplate.awesome()).to.equal('awesome');
-    tdigitalNodeBoilerplate.awesome().should.equal('awesome');
+  describe('Client creation', function() {
+    it('should be able to create a steel client');
+
+    it('should be able to configure where the client should connect');
   });
+
+  describe('Chopping', function() {
+    it('should be able to chop steel');
+
+    it('should inform when a chop has happened');
+
+    it('should inform when a chop has not been successful');
+
+  });
+
 });
